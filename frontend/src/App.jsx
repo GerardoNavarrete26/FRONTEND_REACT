9138bd583b1e91b168d283efb33cada71f529726
Gerardo Navarrete <gerardo.navarrete@genstudents.org>
2025-01-24 16:39:28 -0300

    Avance_Habitaciones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
+import React from 'react';
+import ListaCabañas from './pages/Cabañas/ListaCabañas'; // Importa el componente ListarCabañas
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css'
-
-function App() {
-
-
+const App = () => {
   return (
-    <>
+    <div className="content">
      
-    </>
-  )
-}
+      
+      {/* Solo llamas al componente ListaCabañas */}
+      <ListaCabañas />
+    </div>
+  );
+};
 
-export default App
+export default App;
